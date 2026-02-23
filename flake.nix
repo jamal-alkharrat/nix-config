081e089006d18ca3532ac94607be3ecbe182dc61
Jamal Alkharrat <jamal.alkharrat@gmail.com>
2026-02-23 23:29:53 +0100

    Add configuration to allow unfree packages in nixpkgs

--- flake.nix
+++ flake.nix
@@ -19,6 +19,11 @@
         ];
 
 
+
+      nixpkgs.config = {
+        allowUnfree = true;
+      };
+      
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
